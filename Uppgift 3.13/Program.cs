6f90316904abdede3c295ea45beac1bee5c59d19
leonkarlsson1 <leka05001@utb.vaxjo.se>
2022-09-30 10:23:10 +0200

    Add project files.

---
+++ Uppgift 3.13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift3_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gamal är du?");
+            int svar = int.Parse(Console.ReadLine());
+
+            if (svar < 16)
+                Console.WriteLine("Du får inte delta i tavling");
+            else if (svar > 19)
+                Console.WriteLine("Du får inte delta i tavling");
+            else if (svar >= 16 && svar <= 19)
+                Console.WriteLine("Du får delta i tavling");
+        }
+    }
+}
